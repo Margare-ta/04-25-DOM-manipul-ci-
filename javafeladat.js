97f4965f27fb12aeb6805d8a64025ae7e725b466
Margareta <margaretavida120@gmail.com>
2021-04-26 17:45:54 +0200

    jsjsj

--- javafeladat.js
+++ javafeladat.js
@@ -1,14 +1,11 @@
+let change = document.getElementById('change');
 
+change.addEventListener("click", colorchange);
 
-
-
-
-const setBg = () => {
-  const changehere = 
-  Math.floor(Math.random()*16777215).toString(16);
-  document.button.style.backgroundcolor = "#" + changehere;
-  color.innerHTML = "#" + changehere;
-}
-
-change.addEventListener("click", setBg);
-setBg();
+function colorchange() {
+  let red = Math.floor(Math.random() * 256);
+  let blou = Math.floor(Math.random() * 256);
+  let green = Math.floor(Math.random() * 256);
+  let szin = "background-color: rgb(" + red + ", " + blou + ", " + green + "); ";
+  change.style = szin;
+};
